00f57d34a0a58268e89be839a24f6d2a1a60b0df
pragyanroy <roy.pragyan@gmail.com>
2016-09-13 18:32:30 -0700

    resumeonline

---
+++ js/resumeBuilder.js
@@ -0,0 +1,279 @@
+var bio = { 
+	        "name": "Pragyan Roy",
+            "role": "CISCO UCCE/IVR/ICM and Web Developer",
+             "contacts" : {
+             "mobile": "2036454699",
+             "email": "roy.pragyan@gmail.com",
+             "github": "pragyanroy",
+             "location": "San Francisco"
+         },
+         "welcomeMessage": " Welcome to Pragyan Roy's Resume",
+         "skills": ["CiscoIVR","Cisco ICM","HTML-CSS-JavaScript","Cisco-UCCE"],
+         "bioPic":"images/fry.jpg"
+}
+
+var education={
+	"schools":[
+	{
+		"name": "Biju Patnaik Institute of Technology",
+		"location": "Bhubaneswar India",
+		"degree":"Masters in Computer Science Engineering",
+		"majors":["Computer Science"],
+		"dates":"2004",
+		"url":"http://www.bput.ac.in/"
+	},
+	{
+		"name": "ISC",
+		"location": "Sunabeda India",
+		"degree":"High School Diploma",
+		"majors":["Science"],
+		"dates":"2000",
+		"url":"http://www.cisce.org/"
+	
+	}
+	],
+    "OnlineCourses":[
+    {
+    	"title":"Web Development",
+    	"school":"Udacity",
+    	"dates":"2016",
+    	"url":"https://udacity.com"
+    },
+    {
+    	"title":"CVP Training",
+    	"school":"Sunset learning",
+    	"dates":"2016",
+    	"url":"https://udacity.com"
+    },
+    {
+    	"title":"CCNA Voice",
+    	"school":"Unitek Education",
+    	"dates":"2016",
+    	"url":"https://udacity.com"
+    }
+
+
+    ]
+    }
+
+var work={
+
+	"jobs":[
+	{
+		"employer":"Esurance",
+		"title":"Manager Development",
+		"dates":"April 2014 -Future",
+		"location":"San Fransciso,CA",
+		"description":"Developed a team of CISCO UCCE IVR and CISCO ICM call routing proffesionals.Hands on Manager with Expertise in Cisco Call Studio application,Core Java,Integretion with Soap Webservices,Expertise in developing and troublshooting Cisco ICM"
+
+	},
+		{
+		"employer":"Accenture UK",
+		"title":"Consultant Service Transformation",
+		"dates":"Sep 2010 -April 2012",
+		"location":"London,UK",
+		"description":"Responsible for migrating legacy IVR's and contact center of fortune 500 clients(RoyalBankofScotland) to state of art SIP based contact centers."
+	},
+	{
+		"employer":"Accenture USA",
+		"title":"Consultant Service Transformation",
+		"dates":"Sep 2012 -April 2014",
+		"location":"Bloomington,IL",
+		"description":"Responsible for migrating legacy IVR's and contact center of fortune 500 clients(StateFarm) to state of art SIP based contact centers."
+	},
+ {
+ 	"employer":"TATA Communications",
+		"title":"Assitant Manager Network Voice",
+		"dates":"Oct 2009 -Sep 2010",
+		"location":"Mumbai,India",
+		"description":"Responsible for Designing,Planning and implementing 5000 seater Hosted Contact centers across India.Expertise in Cosmocom,Cisco and Avaya products.Responsible for Project management and execution of IVR and infrastructure projects."
+
+ },
+ {
+ 	"employer":"AVAYA",
+		"title":"Software Engineer",
+		"dates":"Oct 2008 -Oct 2009",
+		"location":"Pune,India",
+		"description":"Responsible for tier 3 and Development support for AVAYA Voice portal IVR for fortune 500 clients of AVAYA.Expertise in AVAYA Voice Portal and integretion with Cisco products"
+ },
+ {
+ 		"employer":"Virstra -I - Technology Services",
+		"title":"Senior Customer Interaction Center Engineer",
+		"dates":"Oct 2006 -Oct 2008",
+		"location":"Tokyo,Japan",
+		"description":"Responsible for Development of IVR and call Routing for Shinsei Bank Call centersin Tokyo.Responsible for planning ,designing and implementing SIP based Interative intelligence contact center"
+ },
+ {
+ 	"employer":"Servion Global Solutions",
+		"title":"Software Engineer",
+		"dates":"June 2005 -Oct 2006",
+		"location":"Chennai, India",
+		"description":"Responsible for Development of IVR and Routing for Banking and Insurance clients"
+ },
+ {
+ 	"employer":"ABN Amro Bank",
+		"title":"Investment Banking officer",
+		"dates":" December 2004 -June 2005",
+		"location":"Chennai,India",
+		"description":" Fixed Income and Derivatives back office "
+ }
+	]
+}
+
+var projects={
+
+	"project": [
+	{
+     "title":"Portfolio Page",
+     "dates":"2016",
+     "description":"Portfolio Page describes my web devlopment projects"
+	},
+    {
+     "title":"Esurance IVR",
+     "dates":"2014-2016",
+     "description":"Please dial 800Esurance and hear the menu options developed by me",
+     "images":"https://www.google.com/search?q=ivr+images&espv=2&biw=1152&bih=652&tbm=isch&imgil=yTnY9qb7TQL7NM%253A%253BedlWMVz_8w6BGM%253Bhttp%25253A%25252F%25252Fwww.elisiontec.com%25252Fproduct%25252Fivr-system%25252F&source=iu&pf=m&fir=yTnY9qb7TQL7NM%253A%252CedlWMVz_8w6BGM%252C_&usg=__tlYv4KEpI20m4AzDqYuAeNx5NOY%3D&ved=0ahUKEwiowdb1rOrOAhVM7GMKHZkKBwUQyjcINQ&ei=fB7GV6iTOMzYjwOZlZwo#imgrc=yTnY9qb7TQL7NM%3A"
+	},
+	{
+     "title":"StaeFarm Bank IVR",
+     "dates":"2012-2014",
+     "description":"Team member for StateFarms's Natural Language IVR projectß",
+     "images":"https://www.google.com/search?q=ivr+images&espv=2&biw=1152&bih=652&tbm=isch&imgil=yTnY9qb7TQL7NM%253A%253BedlWMVz_8w6BGM%253Bhttp%25253A%25252F%25252Fwww.elisiontec.com%25252Fproduct%25252Fivr-system%25252F&source=iu&pf=m&fir=yTnY9qb7TQL7NM%253A%252CedlWMVz_8w6BGM%252C_&usg=__tlYv4KEpI20m4AzDqYuAeNx5NOY%3D&ved=0ahUKEwiowdb1rOrOAhVM7GMKHZkKBwUQyjcINQ&ei=fB7GV6iTOMzYjwOZlZwo#imgrc=yTnY9qb7TQL7NM%3A"
+	},
+	{
+     "title":"Royal Bank of Scotland",
+     "dates":"2010-2012",
+     "description":"Team member for Royal Bank of Scotland's Network transformation project",
+     "images":"https://www.google.com/search?q=ivr+images&espv=2&biw=1152&bih=652&tbm=isch&imgil=yTnY9qb7TQL7NM%253A%253BedlWMVz_8w6BGM%253Bhttp%25253A%25252F%25252Fwww.elisiontec.com%25252Fproduct%25252Fivr-system%25252F&source=iu&pf=m&fir=yTnY9qb7TQL7NM%253A%252CedlWMVz_8w6BGM%252C_&usg=__tlYv4KEpI20m4AzDqYuAeNx5NOY%3D&ved=0ahUKEwiowdb1rOrOAhVM7GMKHZkKBwUQyjcINQ&ei=fB7GV6iTOMzYjwOZlZwo#imgrc=yTnY9qb7TQL7NM%3A"
+	},
+	]
+}
+
+
+
+function inName(name){
+
+	name=name.trim().split(" ");
+	console.log(name);
+	name[1]=name[1].toUpperCase();
+	name[0].slice(0,1).toUpperCase();+name[0].slice(1).toLowerCase();
+	return name[0] + " "+name[1];}
+
+
+
+bio.display=function(){
+/*function displayBio() {*/
+  var formattedName = HTMLheaderName.replace("%data%", bio.name);
+  var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+  $("#header").prepend(formattedRole);
+  $("#header").prepend(formattedName);
+  /*var formattedNameRole = formattedName + formattedRole;
+  $("#header").append(formattedNameRole);*/
+
+
+ 
+  var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+  var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+  var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+ var formattedlocation= HTMLlocation.replace("%data%", bio.contacts.location);
+
+  $('#topContacts').append(formattedMobile);
+  $('#topContacts').append(formattedEmail);
+  $('#topContacts').append(formattedGithub);
+  $('#topContacts').append(formattedlocation);
+
+  var formattedbioPic = HTMLbioPic.replace("%data%", bio.biopic);
+  $("#header").append(formattedbioPic);
+/*var formattedContacts = formattedEmail + formattedMobile;
+  $("#header").append(formattedContacts);*/
+    var formattedwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+   $("#header").append(formattedwelcomeMsg);
+   $("#header").append(HTMLskillsStart);
+	var formattedSkill=HTMLskills.replace("%data%",bio.skills[0]);
+	
+
+}
+
+
+$(document).click(function(loc){
+	var x=loc.pageX;
+	var y=loc.pageY;
+	logClicks(x,y);
+});
+
+function displayWork(){
+
+
+
+for (job in work.jobs){
+	$("#workExperience").append(HTMLworkStart);
+	var formattedEmployer=HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+	var formattedTitle=HTMLworkTitle.replace("%data%",work.jobs[job].title);
+	var formattedEmployerTitle= formattedEmployer+formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
+	var formattedDates=HTMLworkDates.replace("%data%",work.jobs[job].dates);
+	$(".work-entry-last").append(formattedDates);
+	var formattedDescription=HTMLworkDescription.replace("%data%",work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+}
+}
+
+
+
+projects.display=function(){
+
+	for(project in projects.projects)
+	{
+		$("#projects").append(HTMLprojectStart);
+		var formattedTitle=HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+		var formattedDates=HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription=HTMLprojectDates.replace("%data%",project.projects[porject].description);
+		$(".project-entry:last").append(formattedDescription);
+	    if(projects.projects[project].images.length>0)
+	    {
+	      for (image in project.projects[project].images)
+	      {
+	      var formattedImage=HTMLprojectImage.replace("%data%",project.projects[project].images[image]);
+	      $(".project-entry:last").append(formattedImage);
+	      }
+	    }
+	}
+}
+
+
+/*function displayProjects()
+{
+
+	for(project in projects.projects)
+	{
+		$("#projects").append(HTMLprojectStart);
+		var formattedTitle=HTMLprojectTitle.replace("%data%",project.projects[projects].title);
+		$(".project-entry:last").append(formattedTitle);
+		var formattedDates=HTMLprojectDates.replace("%data%",project.projects[projects].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription=HTMLprojectDates.replace("%data%",project.projects[description].dates);
+		$(".project-entry:last").append(formattedDescription);
+	    if(projects.projects[projects].images.length>0)
+	    {
+	      for (image in project.projects[projects].images)
+	      {
+	      var formattedImage=HTMLprojectImage.replace("%data%",project.projects[projects].images[image]);
+	      $(".project-entry:last").append(formattedImage);
+	      }
+	    }
+
+
+	}
+
+
+}*/
+
+$("#mapDiv").append(googleMap);
+/*displayBio();*/
+displayWork();
+bio.display()
+projects.display();
